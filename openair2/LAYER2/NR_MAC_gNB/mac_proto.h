95a39f986e28e99201e82d833220bf6c4662c26c
Robert Schmidt <robert.schmidt@eurecom.fr>
2020-12-10 15:43:19 +0100

    Remove (unnecessary) num_slots_per_tdd in DLSCH sched

--- openair2/LAYER2/NR_MAC_gNB/mac_proto.h
+++ openair2/LAYER2/NR_MAC_gNB/mac_proto.h
@@ -77,14 +77,12 @@ int nr_generate_dlsch_pdu(module_id_t Mod_idP,
 
 void nr_schedule_ue_spec(module_id_t module_id,
                          frame_t frame,
-                         sub_frame_t slot,
-                         int num_slots_per_tdd);
+                         sub_frame_t slot);
 
 /* \brief default preprocessor */
 void nr_simple_dlsch_preprocessor(module_id_t module_id,
                                   frame_t frame,
-                                  sub_frame_t slot,
-                                  int num_slots_per_tdd);
+                                  sub_frame_t slot);
 
 void schedule_nr_mib(module_id_t module_idP, frame_t frameP, sub_frame_t subframeP, uint8_t slots_per_frame);
 
@@ -150,8 +148,7 @@ uint16_t nr_mac_compute_RIV(uint16_t N_RB_DL, uint16_t RBstart, uint16_t Lcrbs);
  * freq resources */
 void nr_preprocessor_phytest(module_id_t module_id,
                              frame_t frame,
-                             sub_frame_t slot,
-                             int num_slots_per_tdd);
+                             sub_frame_t slot);
 /* \brief UL preprocessor for phytest: schedules UE_id 0 with fixed MCS on a
  * fixed set of resources */
 void nr_ul_preprocessor_phytest(module_id_t module_id,
