4aa558ee939a8736cf77b7fceb26387a97adbf05
Robert Schmidt <robert.schmidt@eurecom.fr>
2021-01-06 14:28:50 +0100

    Save TB in MAC, transmit TX_req also on retx, use ptr instead of direct in nFAPI

--- openair1/SCHED_NR/fapi_nr_l1.c
+++ openair1/SCHED_NR/fapi_nr_l1.c
@@ -192,7 +192,7 @@ void nr_schedule_response(NR_Sched_Rsp_t *Sched_INFO){
         uint16_t pduIndex = pdsch_pdu_rel15->pduIndex;
 	AssertFatal(TX_req->pdu_list[pduIndex].num_TLV == 1, "TX_req->pdu_list[%d].num_TLV %d != 1\n",
 		    pduIndex,TX_req->pdu_list[pduIndex].num_TLV);
-        uint8_t *sdu = (uint8_t *)TX_req->pdu_list[pduIndex].TLVs[0].value.direct;
+        uint8_t *sdu = (uint8_t *)TX_req->pdu_list[pduIndex].TLVs[0].value.ptr;
         handle_nr_nfapi_pdsch_pdu(gNB,frame,slot,&dl_tti_pdu->pdsch_pdu, sdu);
       }
     }
