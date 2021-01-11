0ec08738a5204a46bb5def4c5f65ebe7fe46b30d
Sakthivel Velumani <velumani@eurecom.fr>
2021-01-11 14:40:02 +0100

    send data from MAC to PHY when NDI toggles

--- openair2/LAYER2/NR_MAC_UE/mac_defs.h
+++ openair2/LAYER2/NR_MAC_UE/mac_defs.h
@@ -250,6 +250,8 @@ typedef struct {
   uint8_t RA_BI_found;
   /// Flag for the Msg1 generation: enabled at every occurrence of nr prach slot
   uint8_t generate_nr_prach;
+  /// Last NDI of UL HARQ processes
+  uint8_t UL_ndi[NR_MAX_HARQ_PROCESSES];
 
   ////	FAPI-like interface message
   fapi_nr_ul_config_request_t *ul_config_request;
