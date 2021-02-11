245a6da120334ec5bcf62e9e63d87a164e0b615b
Robert Schmidt <robert.schmidt@eurecom.fr>
2021-02-11 08:52:03 +0100

    On resource depletion: do not abort ULSCH scheduling

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
@@ -613,7 +613,7 @@ void pf_ul(module_id_t module_id,
       if (rbStart + sched_pusch->rbSize >= bwpSize) {
         LOG_D(MAC, "cannot allocate retransmission of UE %d/RNTI %04x: no resources\n",
               UE_id, UE_info->rnti[UE_id]);
-        return;
+        continue;
       }
       sched_pusch->rbStart = rbStart;
 
@@ -670,7 +670,7 @@ void pf_ul(module_id_t module_id,
       if (rbStart + min_rb >= bwpSize) {
         LOG_D(MAC, "cannot allocate continuous data for UE %d/RNTI %04x: no resources\n",
               UE_id, UE_info->rnti[UE_id]);
-        return;
+        continue;
       }
       sched_pusch->rbStart = rbStart;
       sched_pusch->rbSize = min_rb;
@@ -752,7 +752,7 @@ void pf_ul(module_id_t module_id,
     if (rbStart + min_rb >= bwpSize) {
       LOG_D(MAC, "cannot allocate UL data for UE %d/RNTI %04x: no resources\n",
             UE_id, UE_info->rnti[UE_id]);
-      return;
+      continue;
     }
 
     /* Calculate the current scheduling bytes */
