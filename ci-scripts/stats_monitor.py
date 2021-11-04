bd56fd846d365aa6bb3e0d9acfadd330b6ce39d0
hardy <remi.hardy@openairinterface.org>
2021-11-04 18:08:03 +0100

    test : add some rt stats to the monitor (graph)

--- ci-scripts/stats_monitor.py
+++ ci-scripts/stats_monitor.py
@@ -35,7 +35,15 @@ class StatMonitor():
                 self.d[node_type]['ulsch_err'].append(int(result.group(3)))
                 percentage=float(result.group(2))/float(result.group(1))
                 self.d[node_type]['ulsch_err_perc_round_1'].append(percentage)
-
+            result=re.match(r'^.*\bL1 Tx processing thread 0\b:\s+([0-9\.]+) us;\s+([0-9]+);\s+([0-9\.]+) us;',tmp)
+            if result is not None:
+                self.d[node_type]['L1 Tx processing thread 0'].append(int(result.group(3)))
+            result=re.match(r'^.*\bL1 Tx processing thread 1\b:\s+([0-9\.]+) us;\s+([0-9]+);\s+([0-9\.]+) us;',tmp)
+            if result is not None:
+                self.d[node_type]['L1 Tx processing thread 1'].append(int(result.group(3)))
+            result=re.match(r'^.*\bDLSCH encoding\b:\s+([0-9\.]+) us;\s+([0-9]+);\s+([0-9\.]+) us;',tmp)
+            if result is not None:
+                self.d[node_type]['DLSCH encoding'].append(int(result.group(3)))
 
     def process_enb (self,node_type,output):
         for line in output:
